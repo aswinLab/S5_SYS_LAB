bd59d5aba5c2062b17c997c5f9a7749669b5be1e
aswinLab <aswinganga777@gmail.com>
2025-07-28 10:26:48 +0530

    28/7/25

--- cscan.c
+++ cscan.c
@@ -1,5 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-	printf("hello world\n");
+int main() {
+    int i, j, temp, n, h, req[30], seek = 0, pos, disk = 200, dir;
+
+    printf("Enter the head position: ");
+    scanf("%d", &h);
+
+    printf("Enter the number of requests: ");
+    scanf("%d", &n);
+
+    printf("Enter the requests: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &req[i]);
+    }
+
+    // Add head, start, and end tracks
+    req[n] = h;
+    req[n + 1] = 0;
+    req[n + 2] = disk - 1;
+    n += 3;
+
+    // Sort the requests
+    for (i = 0; i < n -1; i++) {
+        for (j = i +1; j < n; j++) {
+            if (req[i] > req[j]) {
+                temp = req[i];
+                req[i] = req[j];
+                req[j] = temp;
+            }
+        }
+    }
+
+    // Find position of head in sorted array
+    for (pos = 0; pos < n; pos++) {
+        if (req[pos] == h) {
+            break;
+        }
+    }
+
+    printf("Enter direction for C-SCAN (0 for right, 1 for left): ");
+    scanf("%d", &dir);
+
+    printf("\nSeek Sequence:\n");
+
+    if (dir == 0) {
+        // Move right from head to end
+        for (i = pos; i < n -1; i++) {
+            printf("%d -> ", req[i]);
+            seek += abs(req[i + 1] - req[i]);
+        }
+        printf("%d\n", req[n - 1]);
+
+        // Jump from end to start
+        seek += abs((disk - 1) - 0);
+        printf("Jump from %d to %d\n", disk -1, 0);
+
+        // Move right from start to head-1
+        for (i = 0; i < pos -1; i++) {
+            printf("%d -> ", req[i]);
+            seek += abs(req[i + 1] - req[i]);
+        }
+        if (pos -1 >= 0) printf("%d\n", req[pos -1]);
+    }
+    else if (dir == 1) {
+        // Move left from head to start
+        for (i = pos; i > 0; i--) {
+            printf("%d -> ", req[i]);
+            seek += abs(req[i] - req[i - 1]);
+        }
+        printf("%d\n", req[0]);
+
+        // Jump from start to end
+        seek += abs((disk - 1) - 0);
+        printf("Jump from %d to %d\n", 0, disk - 1);
+
+        // Move left from end to head+1
+        for (i = n -1; i > pos +1; i--) {
+            printf("%d -> ", req[i]);
+            seek += abs(req[i] - req[i -1]);
+        }
+        if (pos +1 < n) printf("%d\n", req[pos +1]);
+    }
+    else {
+        printf("Invalid direction input.\n");
+        return 1;
+    }
+
+    printf("\nTotal Seek Time: %d\n", seek);
+    return 0;
 }
---
+++ fileorg.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct dir {
+    char dir_name[20];
+    char file[10][20];
+    int findex;  // number of files currently
+};
+
+void create(struct dir *d) {
+    if(d->findex < 10){
+        printf("Enter filename: ");
+        scanf("%19s", d->file[d->findex]);
+        d->findex++;
+        printf("File created!\n");
+    } else {
+        printf("No more space available :(\n");
+    }
+}
+
+void deleteFile(struct dir *d) {
+    if(d->findex == 0){
+        printf("No files to delete.\n");
+        return;
+    }
+    char filename[20];
+    printf("Enter filename to delete: ");
+    scanf("%19s", filename);
+
+    int i, found = 0;
+    for(i = 0; i < d->findex; i++){
+        if(strcmp(d->file[i], filename) == 0){
+            found = 1;
+            break;
+        }
+    }
+    if(found){
+        for(int j = i; j < d->findex - 1; j++){
+            strcpy(d->file[j], d->file[j+1]);
+        }
+        d->findex--;
+        printf("File deleted successfully.\n");
+    } else {
+        printf("File not found.\n");
+    }
+}
+
+void searchFile(struct dir *d) {
+    if(d->findex == 0){
+        printf("No files available.\n");
+        return;
+    }
+    char filename[20];
+    printf("Enter filename to search: ");
+    scanf("%19s", filename);
+
+    for(int i = 0; i < d->findex; i++){
+        if(strcmp(d->file[i], filename) == 0){
+            printf("File %s found. Position : %d\n", filename, d->findex);
+            return;
+        }
+    }
+    printf("File not found.\n");
+}
+
+void listFiles(struct dir *d) {
+    if(d->findex == 0){
+        printf("No files in directory.\n");
+        return;
+    }
+    printf("Files in directory '%s':\n", d->dir_name);
+    for(int i = 0; i < d->findex; i++){
+        printf("%d. %s\n", i+1, d->file[i]);
+    }
+}
+
+void renameFile(struct dir *d) {
+    if(d->findex == 0){
+        printf("No files available to rename.\n");
+        return;
+    }
+    char oldname[20], newname[20];
+    printf("Enter filename to rename: ");
+    scanf("%19s", oldname);
+
+    int i, found = 0;
+    for(i = 0; i < d->findex; i++){
+        if(strcmp(d->file[i], oldname) == 0){
+            found = 1;
+            break;
+        }
+    }
+
+    if(found){
+        printf("Enter new filename: ");
+        scanf("%19s", newname);
+        strcpy(d->file[i], newname);
+        printf("File renamed successfully.\n");
+    } else {
+        printf("File not found.\n");
+    }
+}
+
+int main() {
+    struct dir d;
+    d.findex = 0;
+
+    printf("Enter directory name: ");
+    scanf("%19s", d.dir_name);
+
+    char option[3];  // use string because "rn" option is two chars
+    while(1) {
+        printf("\nOptions:\n");
+        printf("Create a new file - n\nDelete a file - d\nSearch for a file - s\nList all available files - l\nRename a file - rn\nExit - e\n");
+        printf("Enter option: ");
+        scanf("%2s", option);
+
+        if(strcmp(option, "e") == 0) {
+            printf("Exiting...\n");
+            break;
+        } else if(strcmp(option, "n") == 0) {
+            create(&d);
+        } else if(strcmp(option, "d") == 0) {
+            deleteFile(&d);
+        } else if(strcmp(option, "s") == 0) {
+            searchFile(&d);
+        } else if(strcmp(option, "l") == 0) {
+            listFiles(&d);
+        } else if(strcmp(option, "rn") == 0) {
+            renameFile(&d);
+        } else {
+            printf("Invalid option. Try again.\n");
+        }
+    }
+    return 0;
+}
+
